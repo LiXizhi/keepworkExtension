d148ece08ba7264da651b200546428ea4c2ef424
lixizhi <lixizhi@yeah.net>
2026-08-24 11:35:09 +0800

    fix: import OS utilities for user path expansion

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs';
+import * as os from 'node:os';
 import * as path from 'node:path';
 import { defaultUserWorkspace, DEFAULT_WORKSPACE_SLOT, readConfigFile } from './config';
 
